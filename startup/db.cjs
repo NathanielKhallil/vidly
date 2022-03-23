e31f69a82e7cd0110568416eb76517a4ffd805ec
NathanielKhallil <78049880+NathanielKhallil@users.noreply.github.com>
2022-03-23 13:16:53 -0600

    Set up test environment config and created db.cjs

--- startup/db.cjs
+++ startup/db.cjs
@@ -1,11 +1,11 @@
 const winston = require("winston");
 const mongoose = require("mongoose");
+const config = require("config");
 
 const logging = winston.createLogger({
   transports: [new winston.transports.Console()],
 });
 module.exports = function () {
-  mongoose
-    .connect("mongodb://localhost/vidly")
-    .then(() => logging.info("Connected to MongoDB..."));
+  const db = config.get("db");
+  mongoose.connect(db).then(() => logging.info(`Connected to ${db}...`));
 };
