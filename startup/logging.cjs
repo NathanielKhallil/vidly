bcfe57a789a08e9931ab855c0b96a0d2f01f64c6
NathanielKhallil <78049880+NathanielKhallil@users.noreply.github.com>
2022-04-20 16:14:42 -0600

    revert changes

--- startup/logging.cjs
+++ startup/logging.cjs
@@ -34,7 +34,7 @@ module.exports = function (app) {
     transports: [
       new winston.transports.Console({ colorize: true, PrettyPrint: true }),
       new winston.transports.MongoDB({
-        db: "mongodb+srv://ViktorKhallil:1234@cluster0.bwsa3.mongodb.net/test",
+        db: "mongodb://localhost/vidly",
         level: "info",
         format: winston.format.json(),
       }),
